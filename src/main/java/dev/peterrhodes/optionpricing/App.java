2a68d4137f3e3e92ad6c2ac53b33ec4e80767677
prhodesy <peter.rhodes@gmail.com>
2021-09-11 22:13:28 +0700

    created project

---
+++ src/main/java/dev/peterrhodes/optionpricing/App.java
@@ -0,0 +1,13 @@
+package dev.peterrhodes.optionpricing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
