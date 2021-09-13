40bd454638b358747538af5dcbfc108c55c16ce1
prhodesy <peter.rhodes@gmail.com>
2021-09-13 13:26:20 +0700

    made pricer static

--- src/main/java/dev/peterrhodes/optionpricing/pricers/CoxRossRubinsteinPricer.java
+++ src/main/java/dev/peterrhodes/optionpricing/pricers/CoxRossRubinsteinPricer.java
@@ -1,7 +1,6 @@
 package dev.peterrhodes.optionpricing.pricers;
 
 import dev.peterrhodes.optionpricing.core.Option;
-import dev.peterrhodes.optionpricing.core.Pricer;
 import dev.peterrhodes.optionpricing.enums.OptionStyle;
 import dev.peterrhodes.optionpricing.enums.OptionType;
 import dev.peterrhodes.optionpricing.models.CoxRossRubinsteinModel;
@@ -11,41 +10,32 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
-public class CoxRossRubinsteinPricer implements Pricer<CoxRossRubinsteinModel> {
-
-    private int timeSteps;
-
-    /**
-     * TODO
-     * @throws IllegalArgumentException if timeSteps is not greater than zero
-     */
-    public CoxRossRubinsteinPricer(int timeSteps) throws IllegalArgumentException {
-        if (timeSteps <= 0) {
-            throw new IllegalArgumentException("timeSteps must be greater than zero");
-        }
-        this.timeSteps = timeSteps;
-    }
+public abstract class CoxRossRubinsteinPricer {
 
     //region price
     //----------------------------------------------------------------------
 
     /**
-     * {@inheritDoc}
+     * TODO
+     * @throws IllegalArgumentException if timeSteps is not greater than zero
      */
-    @Override
-    public double price(Option option) {
+    public static double price(Option option, int timeSteps) throws IllegalArgumentException {
+        if (timeSteps <= 0) {
+            throw new IllegalArgumentException("timeSteps must be greater than zero");
+        }
+
         // Determine parameters
-        double Δt = option.getT() / (double)(this.timeSteps); // length of a single time interval/step
+        double Δt = option.getT() / timeSteps; // length of a single time interval/step
         double u = Math.exp(option.getV() * Math.sqrt(Δt)); // proportional up movement
         double d = Math.exp(-option.getV() * Math.sqrt(Δt)); // proportional down movement
         double a = Math.exp((option.getR() - option.getQ()) * Δt); // growth factor
         double p = (a - d) / (u - d); // probability of an up movement (probability of a down movement is 1 - p)
         //System.out.println("Δt = " + Δt + ", u = " + u + ", d = " + d + ", p = " + p);
 
-        return this.price(option, Δt, u, d, p);
+        return CoxRossRubinsteinPricer.price(option, timeSteps, Δt, u, d, p);
     }
 
-    private class Node {
+    private static class Node {
         public double S;
         public double V;
         public Node(double S, double V) {
@@ -54,16 +44,16 @@ public class CoxRossRubinsteinPricer implements Pricer<CoxRossRubinsteinModel> {
         }
     }
 
-    private double price(Option option, double Δt, double u, double d, double p) {
+    private static double price(Option option, int timeSteps, double Δt, double u, double d, double p) {
         List<Node> nodes = new ArrayList(); // key: ith time step, nth node
 
         // TODO calc underlying prices
-        for (int i = 0; i <= this.timeSteps; i++) { // ith time step: time = iΔt (i = 0, 1, ..., time steps)
+        for (int i = 0; i <= timeSteps; i++) { // ith time step: time = iΔt (i = 0, 1, ..., time steps)
             for (int n = 0; n <= i; n++) { // nth node (from lowest underlying price to highest): underlying price = S₀uⁿdⁱ⁻ⁿ
                 double S = option.getS() * Math.pow(u, n) * Math.pow(d, i - n);
                 //double t = i == this.timeSteps ? option.getT() : i * Δt;
                 double V;
-                if (i == this.timeSteps) {
+                if (i == timeSteps) {
                     V = Math.max(0.0, option.getType() == OptionType.CALL ? S - option.getK() : option.getK() - S);
                 } else {
                     switch (option.getStyle()) {
@@ -85,7 +75,7 @@ public class CoxRossRubinsteinPricer implements Pricer<CoxRossRubinsteinModel> {
         }
 
         // TODO calc option values
-        for (int i = this.timeSteps - 1; i >= 0; i--) {
+        for (int i = timeSteps - 1; i >= 0; i--) {
             for (int n = 0; n <= i; n++) {
                 int currentIndex = IntStream.rangeClosed(0, i).sum() + n;
                 int downIndex = currentIndex + (i + 1);
@@ -104,8 +94,7 @@ public class CoxRossRubinsteinPricer implements Pricer<CoxRossRubinsteinModel> {
     /**
      * TODO
      */
-    @Override
-    public CoxRossRubinsteinModel calculation(Option option) throws NotImplementedException {
+    public static CoxRossRubinsteinModel calculation(Option option) throws NotImplementedException {
         throw new NotImplementedException();
     }
 }
--- src/test/java/dev/peterrhodes/optionpricing/pricers/CoxRossRubinsteinPricerTest.java
+++ src/test/java/dev/peterrhodes/optionpricing/pricers/CoxRossRubinsteinPricerTest.java
@@ -20,9 +20,12 @@ class CoxRossRubinsteinPricerTest {
 
     @Test
     void zeroTimeSteps() {
-        // Arrange Act Assert
+        // Arrange
+        EuropeanOption option = new EuropeanOption(OptionType.CALL, 810, 800, 0.5, 0.2, 0.05, 0.02);
+
+        // Act Assert
         assertThatThrownBy(() -> {
-            CoxRossRubinsteinPricer ex = new CoxRossRubinsteinPricer(0);
+            double ex = CoxRossRubinsteinPricer.price(option, 0);
         }).isInstanceOf(IllegalArgumentException.class)
           .hasMessageContaining(this.greaterThanZeroMessage);
     }
@@ -34,35 +37,48 @@ class CoxRossRubinsteinPricerTest {
     //----------------------------------------------------------------------
 
     /*
-     * Hull: page 313, section 13.11, Example 13.1
-     */
-    @Test
-    void price_Hull_Ex13_1() {
-        // Arrange
-        EuropeanOption option = new EuropeanOption(OptionType.CALL, 810, 800, 0.5, 0.2, 0.05, 0.02);
-        CoxRossRubinsteinPricer crrPricer = new CoxRossRubinsteinPricer(2);
-
-        // Act
-        double result = crrPricer.price(option);
-
-        // Assert
-        assertThat(result).isEqualTo(53.39, withPrecision(0.01));
-    }
-
-    /*
      * Hull: page 311, section 13.9, Figure 13.10
      */
     @Test
     void price_Hull_Fig13_10() {
         // Arrange
         ExoticOption option = new ExoticOption(OptionStyle.AMERICAN, OptionType.PUT, 50, 52, 2, 0.3, 0.05, 0);
-        CoxRossRubinsteinPricer crrPricer = new CoxRossRubinsteinPricer(2);
 
         // Act
-        double result = crrPricer.price(option);
+        double result = CoxRossRubinsteinPricer.price(option, 2);
 
         // Assert
         assertThat(result).isEqualTo(7.43, withPrecision(0.01));
+    }
+
+    /*
+     * Hull: page 313, section 13.11, Example 13.1
+     */
+    @Test
+    void price_Hull_Ex13_1() {
+        // Arrange
+        EuropeanOption option = new EuropeanOption(OptionType.CALL, 810, 800, 0.5, 0.2, 0.05, 0.02);
+
+        // Act
+        double result = CoxRossRubinsteinPricer.price(option, 2);
+
+        // Assert
+        assertThat(result).isEqualTo(53.39, withPrecision(0.01));
+    }
+
+    /*
+     * Hull: page 314, section 13.11, Example 13.2
+     */
+    @Test
+    void price_Hull_Ex13_2() {
+        // Arrange
+        ExoticOption option = new ExoticOption(OptionStyle.AMERICAN, OptionType.CALL, 0.6100, 0.6000, 0.25, 0.12, 0.05, 0.07);
+
+        // Act
+        double result = CoxRossRubinsteinPricer.price(option, 3);
+
+        // Assert
+        assertThat(result).isEqualTo(0.019, withPrecision(0.001));
     }
 
     //----------------------------------------------------------------------
